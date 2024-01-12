4c3525b33d1bc708ec25c4e87458b29376060661
E4SYFLAME <E4SYFLAME@gmail.com>
2024-01-12 17:32:32 -0500

    added second scene to main menu

--- gameproj/main_menu/main_menu.gd
+++ gameproj/main_menu/main_menu.gd
@@ -1,16 +1,22 @@
 class_name MainMenu
 extends Control
 
-@onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
+@onready var spread_please_start = $MarginContainer/HBoxContainer/VBoxContainer/Spread_Please_Start as Button
+@onready var bad_grass_start = $MarginContainer/HBoxContainer/VBoxContainer/Bad_Grass_Start as Button
 @onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
-@onready var start_level = preload("res://Spread_Please/spread_please.tscn") as PackedScene
+@onready var start_level_one = preload("res://SpreadPlease/spread_please.tscn") as PackedScene
+@onready var start_level_two = preload("res://BadGrass/grassgame.tscn") as PackedScene
 
 func _ready():
-	start_button.button_down.connect(on_start_pressed)
+	spread_please_start.button_down.connect(on_start_pressed_one)
+	bad_grass_start.button_down.connect(on_start_pressed_two)
 	exit_button.button_down.connect(on_exit_pressed)
 
-func on_start_pressed() -> void:
-	get_tree().change_scene_to_packed(start_level)
+func on_start_pressed_one() -> void:
+	get_tree().change_scene_to_packed(start_level_one)
+
+func on_start_pressed_two() -> void:
+	get_tree().change_scene_to_packed(start_level_two)
 
 func on_exit_pressed() -> void:
 	get_tree().quit()
