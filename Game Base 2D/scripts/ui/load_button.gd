e5aedfe8fb0ac2237d2bc9456191adfafb9485df
Neptune <ultra.neppers@gmail.com>
2024-01-12 11:21:45 -0700

    Just going with a completely blank project instead

--- Game Base 2D/scripts/ui/load_button.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-func _ready():
-	disabled = not SaveManager.has_save_file()
-
-func _pressed():
-	SaveManager.load_game()
--- Game Base 2D/scripts/ui/viewport_texture_rect.gd
+++
@@ -1,7 +0,0 @@
-class_name ViewportTextureRect extends TextureRect
-
-
-@export var viewport : SubViewport
-
-func _ready():
-	texture = viewport.get_texture()
