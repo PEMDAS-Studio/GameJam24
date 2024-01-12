9d1b71a2432e6673da1a2f116e788a7e4d7e65df
Moaz <ccccdddd96@gmail.com>
2024-01-12 22:44:25 +0000

    Added character Scene with movements.Adjusted the screen sizes and scaling mode

---
+++ gameproj/Character.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+@export var SPEED = 72.0
+@onready var sprite = $Sprite2D as AnimatedSprite2D
+
+func _physics_process(delta):
+	var xDirection = Input.get_axis("left", "right")
+	var yDirection = Input.get_axis("up", "down")
+	
+	if xDirection:
+		velocity.x = xDirection * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if yDirection:
+		velocity.y = yDirection * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+	var normalizedSpeed = velocity.normalized()
+	velocity = SPEED * normalizedSpeed
+	
+	HandleAnimation()
+	
+	move_and_slide()
+
+func HandleAnimation():
+	if (velocity == Vector2.ZERO):
+		sprite.pause()
+		sprite.frame = 0
+	
+	print_debug(velocity)
+	
+	if velocity.x > 0 && velocity.y == 0:
+		sprite.play("Right")
+	elif velocity.x < 0 && velocity.y == 0:
+		sprite.play("Left")
+	
+	if velocity.y > 0:
+		sprite.play("Down")
+	elif velocity.y < 0:
+		sprite.play("Up")
+	
+	
