c35b2dfb091b0c45d658c026983f696a89471a6a
E4SYFLAME <E4SYFLAME@gmail.com>
2024-01-12 20:29:51 -0500

    edited settings menu

--- gameproj/main_menu/main_menu.gd
+++ gameproj/main_menu/main_menu.gd
@@ -3,14 +3,13 @@ extends Control
 
 @onready var spread_please_start = $MarginContainer/VBoxContainer/VBoxContainer2/Spread_Please_Start as Button
 @onready var bad_grass_start = $MarginContainer/VBoxContainer/VBoxContainer2/Bad_Grass_Start as Button
+@onready var setting_button = $MarginContainer/VBoxContainer/VBoxContainer2/Setting_Button as Button
 @onready var exit_button = $MarginContainer/VBoxContainer/VBoxContainer2/Exit_Button as Button
 @onready var start_level_one = preload("res://SpreadPlease/spread_please.tscn") as PackedScene
 @onready var start_level_two = preload("res://BadGrass/grassgame.tscn") as PackedScene
 
 func _ready():
-	spread_please_start.button_down.connect(on_start_pressed_one)
-	bad_grass_start.button_down.connect(on_start_pressed_two)
-	exit_button.button_down.connect(on_exit_pressed)
+	handle_connecting_signals()
 
 func on_start_pressed_one() -> void:
 	get_tree().change_scene_to_packed(start_level_one)
@@ -18,5 +17,14 @@ func on_start_pressed_one() -> void:
 func on_start_pressed_two() -> void:
 	get_tree().change_scene_to_packed(start_level_two)
 
+func on_setting_pressed() -> void:
+	print("LOAD SETTINGS MENU")
+
 func on_exit_pressed() -> void:
 	get_tree().quit()
+
+func handle_connecting_signals() -> void:
+	spread_please_start.button_down.connect(on_start_pressed_one)
+	bad_grass_start.button_down.connect(on_start_pressed_two)
+	setting_button.button_down.connect(on_setting_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
