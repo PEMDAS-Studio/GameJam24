a1261e89e812da34576da4c2f5b018adc5afa976
Moaz <ccccdddd96@gmail.com>
2024-01-24 00:24:49 +0000

    Added Items

--- gameproj/BadGrass/Items/AvailableItems.gd
+++ gameproj/BadGrass/Items/AvailableItems.gd
@@ -1,11 +1,20 @@
 extends Node
 
 var _Items : Array[Resource] = [
-	load("res://BadGrass/Items/SampleItem.tscn"),
-	load("res://BadGrass/Items/SampleItemSpeed.tscn")
+	load("res://BadGrass/Items/Cookie.tscn"),
+	load("res://BadGrass/Items/RegenPotionThree.tscn"),
+	load("res://BadGrass/Items/Fish.tscn"),
+	load("res://BadGrass/Items/SpeedBoostTwo.tscn"),
+	load("res://BadGrass/Items/RegenPotionTwo.tscn"),
+	load("res://BadGrass/Items/CandieStick.tscn"),
+	load("res://BadGrass/Items/RegenPotionOne.tscn"),
+	load("res://BadGrass/Items/CandieBall.tscn"),
+	load("res://BadGrass/Items/SpeedBoostOne.tscn"),
+	load("res://BadGrass/Items/Banana.tscn"),
+	load("res://BadGrass/Items/Apple.tscn"),
 ]
 
-var _ItemDropRates : Array[int] = [68, 65] 
+var _ItemDropRates : Array[int] = [2, 3, 9, 15, 15, 22, 30, 40, 65, 65, 70] 
 
 func GetItem() -> BaseItem:
 	var totalPercentage = _ItemDropRates.reduce(func(accum, number): return accum + number)
