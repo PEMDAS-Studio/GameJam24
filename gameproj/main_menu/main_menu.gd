6671aa9d8ea38d52b6b4e974b0c6a96b5416a970
E4SYFLAME <E4SYFLAME@gmail.com>
2024-01-26 18:31:44 -0500

    added credits screen

--- gameproj/main_menu/main_menu.gd
+++ gameproj/main_menu/main_menu.gd
@@ -4,10 +4,13 @@ class_name MainMenu
 @onready var bad_grass_start = $MarginContainer/VBoxContainer2/Bad_Grass_Start as Button
 @onready var setting_button = $MarginContainer/VBoxContainer2/Setting_Button as Button
 @onready var exit_button = $MarginContainer/VBoxContainer2/Exit_Button as Button
+@onready var credits_button = $Credits_Button as Button
 @onready var start_level = load("res://BadGrass/grassgame.tscn") as PackedScene
 @onready var audioPlayer = $AudioStreamPlayer as AudioStreamPlayer
 @onready var settings_menu = $Settings_Menu as SettingsMenu
+@onready var credits_screen = $Credits_Screen as CreditsScreen
 @onready var settings = load("res://main_menu/Settings Menu/settings_menu.tscn") as PackedScene
+@onready var credits = load("res://main_menu/credits_screen.tscn") as PackedScene
 
 func _ready():
 	AvaibableBuffList.ResetBuffs()
@@ -26,10 +29,15 @@ func on_setting_pressed() -> void:
 	var scene = settings.instantiate()
 	add_child(scene)
 
+func on_credits_pressed() -> void:
+	var scene = credits.instantiate()
+	add_child(scene)
+
 func on_exit_pressed() -> void:
 	get_tree().quit()
 
 func handle_connecting_signals() -> void:
 	bad_grass_start.button_down.connect(on_start_pressed)
 	setting_button.button_down.connect(on_setting_pressed)
+	credits_button.button_down.connect(on_credits_pressed)
 	exit_button.button_down.connect(on_exit_pressed)
