9cfaf3efd30654b7232d2501143b21702125b6c4
Neptune <ultra.neppers@gmail.com>
2024-01-25 18:53:00 -0700

    Fix crash on gameover
    Fixed a bug in the bullet's script that causes a crash on game over

--- gameproj/BadGrass/bullet/bullet_sample.gd
+++ gameproj/BadGrass/bullet/bullet_sample.gd
@@ -40,6 +40,8 @@ func disableDecon():
 	can_decontaminate = false
 
 func Decontaminate():
+	if (!tile_map):
+		pass
 	var tile_pos : Vector2i = tile_map.local_to_map(global_position) / 4
 	var tile_source = tile_map.get_cell_source_id(0, tile_pos);
 	
